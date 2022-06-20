d4bf49a22e2e695b2ea09834b38e8790fee0a98d
AliImran759 <aliimranbocenov@gmail.com>
2022-06-20 21:04:37 +0600

    first commit

---
+++ bin/first_appp.dart
@@ -0,0 +1,5 @@
+import 'package:first_appp/first_appp.dart' as first_appp;
+
+void main(List<String> arguments) {
+  print('Hello world !');
+}
---
+++ test/first_appp_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_appp/first_appp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
